82871c4dc0ce85dc4388d7d6a8cc32d1fbbd409f
zhouqian <katherine.zhou@baracoda.com>
2022-11-28 14:20:38 +0800

    sample app: connectivity and manipulation

--- src/pages/main/Home/index.ts
+++ src/pages/main/Home/index.ts
@@ -1,11 +1,111 @@
-import { findToothbrushes } from '@kolibree/toothbrush-client';
+import {
+  findToothbrushes,
+  stopFindingToothbrushes,
+  DefaultToothbrushWrapper, BrushingMode,
+} from '@kolibree/toothbrush-client';
+import { createToothbrushWrapperFromDeviceId } from "@kolibree/toothbrush-client/lib/device-wrappers";
+import {getToothbrushWrapper} from "@kolibree/toothbrush-client/lib/device-registry";
+
 Page({
+  deviceShadowList: [],
+  data: {
+    toothbrushes: [],
+    connectionStatus: 'disconnected',
+    deviceName: '',
+    deviceId: '',
+    batteryInfo: { batteryLevel: 0 },
+    serialNumber: '',
+    brushingMode: 'clean',
+    vibrating: false,
+  },
   onLoad(){
-    console.log('hello here')
+    console.log('hello here');
+  },
+  onUnload(){
+    this.stopSearchingToothbrush();
+  },
+  onHide() {
+    this.stopSearchingToothbrush();
   },
   searchToothbrush() {
     findToothbrushes().subscribe((device) => {
-        console.log('device shadow:', device);
+      console.log('device shadow:', device);
+      const { deviceId, deviceName } = device;
+      this.setData({
+        toothbrushes: [
+          ...this.data.toothbrushes,
+          { deviceId, deviceName }
+        ]
+      });
+      this.deviceShadowList.push(device);
     })
-  }
-})
+  },
+
+  stopSearchingToothbrush() {
+    stopFindingToothbrushes();
+    console.log('stop finding toothbrushes');
+  },
+
+  handleConnect({currentTarget: {dataset: { id, name }}}) {
+    this.stopSearchingToothbrush();
+    const chosenDevice = this.deviceShadowList.find( (device) => device.deviceId === id );
+    createToothbrushWrapperFromDeviceId(
+      chosenDevice.deviceId,
+      {registerInstance: true}
+    ).then((tb) => {
+      this.setData({
+        toothbrushes: [],
+        connectionStatus: 'connected',
+        deviceName: name,
+        deviceId: id,
+      })
+    });
+  },
+
+  getSerialNumber() {
+    const wrapper = getToothbrushWrapper(this.data.deviceId) as DefaultToothbrushWrapper;
+    wrapper.serialNumber.fetch().then();
+    wrapper.serialNumber.getNotifications$().subscribe((number) => {
+      this.setData({
+        serialNumber: number
+      })
+    })
+  },
+
+  setBrushingMode() {
+    const wrapper = getToothbrushWrapper(this.data.deviceId) as DefaultToothbrushWrapper;
+    wrapper.brushingMode.fetch();
+    let time = + new Date();
+    wrapper.brushingMode.update({ mode: BrushingMode.WHITENING, timestamp: time });
+  },
+
+  getBatteryInfo() {
+    const wrapper = getToothbrushWrapper(this.data.deviceId) as DefaultToothbrushWrapper;
+    wrapper.batteryInfo.toggleSubscribe('1');
+    const subscription = wrapper.batteryInfo.getNotifications$().subscribe((info) => {
+      this.setData({
+        batteryInfo: {
+          batteryLevel: info.batteryLevel
+        }
+      })
+    })
+    if( this.data.batteryInfo.batteryLevel !== 0 ) {
+      subscription.unsubscribe();
+    }
+  },
+
+  toggleVibrator() {
+    const wrapper = getToothbrushWrapper(this.data.deviceId) as DefaultToothbrushWrapper;
+    if (this.data.vibrating === false) {
+      wrapper.toggleVibrator.trigger(true);
+      this.setData({
+        vibrating: true
+      })
+    } else {
+      wrapper.toggleVibrator.trigger(false);
+      this.setData({
+        vibrating: false
+      })
+    }
+  },
+});
